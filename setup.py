b7f6d8b1d612b0e4b2952e635734d9f6b6336180
Grégory Salvan <apieum@gmail.com>
2013-12-04 02:14:18 +0100

    Update version to 0.2 Changes: It's the first functionnal version.

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ import os
 pkgName = 'ducktype'
 setup(
     name=pkgName,
-    version='0.1',
+    version='0.2',
     url='http://www.python.org/pypi/' + pkgName,
     author='Grégory Salvan',
     author_email='apieum@gmail.com',
     license='LGPL',
-    description='Check ducktyped objects',
+    description='Check ducktyping',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
